f408aaff844ccdeaf40ae2ca73f3e0124f61308c
aliii.sss2333 <aliii.sss2333@gmail.com>
2025-03-26 19:46:15 +0000

    TRES

--- MotionPluginAngular/capacitor.config.ts
+++ MotionPluginAngular/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'angular-motion',
-  webDir: 'dist/angular-motion/browser'
+  webDir: 'dist/angular-busta/browser'
 };
 
 export default config;
